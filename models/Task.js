5634f3e36e49d298b823e4a78b00aded6deb7dbf
Julhasur Chowdhury <37052892+julhasur@users.noreply.github.com>
2025-01-15 00:59:13 -0500

    schema created

---
+++ models/Task.js
@@ -0,0 +1,29 @@
+const mongoose= require('mongoose');
+
+const taskSchema=new mongoose.Schema({
+    title: {
+        type: String,
+        required:[`Title is required`],//msut require title
+    },
+    description: {
+        type: String,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'in-progress', 'completed'],
+        default: 'pending'
+    },
+    dueDate:{
+        type: Date,
+    },
+     createdAt: {
+        type: Date,
+        default: Date.now//current date
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports=mongoose.model('Task',taskSchema);
